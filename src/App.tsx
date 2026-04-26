a78b2af7c6e492dcc0c724dbe463d5d009c24c2f
Syahrizal Ardana <msrgombeng@gmail.com>
2026-04-26 12:39:42 +0700

    Add QR code component with styling and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import "./App.css";
+import QRCodeImage from "./assets/image-qr-code.png";
 
 function App() {
   return (
-    <>
-      <h1>Improve your front-end skills by building projects</h1>
-
-      <p>
-        Scan the QR code to visit Frontend Mentor and take your coding skills to
-        the next level
-      </p>
-    </>
+    <div className="container">
+      <div className="card">
+        <img src={QRCodeImage} alt="QR Code Image" className="qr-image" />
+        <div className="container-desc">
+          <h1 className="title">
+            Improve your front-end skills by building projects
+          </h1>
+          <p className="desc">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
